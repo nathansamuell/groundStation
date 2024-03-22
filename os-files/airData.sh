7066460e516880a3782f0398e2289c159117b6fa
Nathan Samuell <nathansamuell@duck.com>
2024-03-21 19:33:09 -0500

    started work on systemd units to handle app startup

---
+++ os-files/airData.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "airData has started!"
+while :
+do
+    echo "airData is running!"
+    sleep 1
+done
---
+++ os-files/gsHelper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "gsHelper has started!"
+while :
+do
+    echo "gsHelper is running!"
+    sleep 1
+done
