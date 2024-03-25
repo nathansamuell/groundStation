152d925cd6e81a93333780c9787c25e2dc9696fa
nathansamuell <nathansamuell@duck.com>
2024-03-25 16:12:51 -0500

    added python capabilities test files

---
+++ tests/refreshWindowBackground.py
@@ -0,0 +1,53 @@
+from PyQt5.QtCore import QSize, Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+
+import sys
+import time
+from random import choice
+import random
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.iterations = 0
+        self.currentColor = QColor(255,255,255)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        # self.refreshWindowTitle = False
+        self.button = QPushButton("Start Refresh Test")
+        self.setWindowTitle("Background Refresh Test")
+        self.button.clicked.connect(self.startTest)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.refresh)
+        self.setCentralWidget(self.button)
+        self.button.setFixedSize(200,50)
+        self.setFixedSize(QSize(800,480))
+
+    def startTest(self):
+            self.timer.start(50)
+            self.iterations = 0
+
+    def refresh(self):
+        if self.iterations < 100:
+            self.currentColor = QColor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+            self.update()
+            self.iterations += 1
+
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        qp.fillRect(self.rect(), self.currentColor)
+
+    def keyPressEvent(self, event):
+            if event.key() == Qt.Key_Escape:
+                self.close()
+
+            print("Escape key pressed. Closing application.")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
---
+++ tests/refreshWindowTitle.py
@@ -0,0 +1,40 @@
+from PyQt5.QtCore import QSize, Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+
+import sys
+import time
+from random import choice
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.window_titles = {i: f"Window Title {i}" for i in range(1, 101)}
+        self.iterations = 0
+
+        # self.refreshWindowTitle = False
+        self.button = QPushButton("Start Refresh Test")
+        self.button.clicked.connect(self.startTest)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.refresh)
+        self.setCentralWidget(self.button)
+        self.setFixedSize(QSize(800,480))
+
+    def startTest(self):
+            self.timer.start(50)
+            # print("Changed window title!");
+
+    def refresh(self):
+        if self.iterations < 100:
+            new_window_title = choice(list(self.window_titles.values()))
+            self.setWindowTitle(new_window_title)
+            self.iterations += 1
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
