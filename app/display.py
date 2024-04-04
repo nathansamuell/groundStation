ec7a84b5fa0de0cfa27e8adc7a8f2d2d42f6074d
Nathan Samuell <nathansamuell@duck.com>
2024-04-03 23:42:55 -0500

    basic display functionality complete

--- app/display.py
+++ app/display.py
@@ -6,3 +6,21 @@
 # More information on the MIT license as well as a complete copy
 # of the license can be found here: https://choosealicense.com/licenses/mit/
 # All above text must be included in any restribution.
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QTextBrowser
+
+
+class DataDisplay(QTextBrowser):
+    def __init__(self):
+        super().__init__()
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.appendText)
+        self.setPlainText("This is the starting message!")
+        self.displayLoop()
+
+    def displayLoop(self):
+        self.timer.start(1000)
+
+    def appendText(self):
+        self.append("This message is appended!")
