186df342180d020d3534097034deb48ab40e50f9
Nathan Samuell <nathansamuell@duck.com>
2024-09-17 23:16:50 -0500

    updated state struct name, added mainwindow test that is not functional

--- tests/test_MainWindow.py
+++ tests/test_MainWindow.py
@@ -12,9 +12,26 @@
 import sys
 import unittest
 from unittest.mock import patch
+from PyQt5.QtTest import QSignalSpy
+
+from groundStation.MainWindow import MainWindow, WindowStatus
+from groundStation.Numpad import Numpad
 
 class TestMainWindow(unittest.TestCase):
-    def testStates(self):
-        # TODO: test the state machine
-        print("check to do")
-        self.assertEqual(1, 1)
+    def test_initial_state(self):
+        mainWindow = MainWindow()
+        stateListener = QSignalSpy(mainWindow.statusSignal)
+        self.assertEqual(len(stateListener), 1) # check for signal emission
+        self.assertEqual(mainWindow.status, WindowStatus.LOGIN) # check for the correct signal
+        stateListener.clear()
+
+    @patch("os.getenv")
+    def test_tsm_login_success(self, mock_env):
+        mainWindow = MainWindow()
+        stateListener = QSignalSpy(mainWindow.statusSignal)
+        mock_env.return_value = "1234"
+        mainWindow.loginWindow.numpad.loginPin = "1234"
+        mainWindow.loginWindow.numpad.authenticate()
+        self.assertEqual(len(stateListener), 1)  # only one signal emitted!
+        self.assertEqual(mainWindow.status, WindowStatus.RAW_TEXT)  # TODO: change to default view once implemented
+        stateListener.clear()
