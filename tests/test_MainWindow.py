a78e1f53bc86135b98eecf7079d0b6ba9fd237b7
nathansamuell <nathansamuell@duck.com>
2024-09-17 22:06:40 -0500

    worked on updating tests and added a new one

---
+++ tests/test_MainWindow.py
@@ -0,0 +1,20 @@
+# SRAD Avionics Ground Software for AIAA UH
+#
+# Copyright (c) 2024 Nathan Samuell (www.github.com/nathansamuell)
+# Licensed under the MIT License
+#
+# More information on the MIT license as well as a complete copy
+# of the license can be found here: https://choosealicense.com/licenses/mit/
+# All above text must be included in any restribution.
+
+
+# imports
+import sys
+import unittest
+from unittest.mock import patch
+
+class TestMainWindow(unittest.TestCase):
+    def testStates(self):
+        # TODO: test the state machine
+        print("check to do")
+        self.assertEqual(1, 1)
