42380e562a77f8d3df7a0f16b559dcff9ba420af
nathansamuell <nathansamuell@duck.com>
2024-10-24 19:16:25 -0500

    cleaning for merge to main branch

--- src/groundStation/SerialCommunicator.py
+++ src/groundStation/SerialCommunicator.py
@@ -15,21 +15,20 @@
 # quote says to "only use exceptions when you cannot know if an operation can succeed or fail"
 # for us, we can't guarantee we'll receive rocket data perfectly reliably -- so we have ample
 # reason to use an exception or try/catch block to handle having an empty queue...for version
-# 1 of the app. 
+# 1 of the app.
 
 # HOWEVER:
 # quote says that "Asking for data can fail. giving data when you have it cannot" at 16:35
 
 # we're currently facing issues trying to read from an empty queue -- the display always checks for data
-# even if the serial communicator doesn't ever receive it... this is an error where we're asking for 
+# even if the serial communicator doesn't ever receive it... this is an error where we're asking for
 # data we don't have!
 
 # shouldn't we refactor so that the serial communicator receiving data prompts a display update?
-# right now, it updates as it reads from a queue. What if we send a signal to update the display 
+# right now, it updates as it reads from a queue. What if we send a signal to update the display
 # containing our data--this should have the same effect. we still constantly update no matter
-# when its received, still solving our original system clock problem, but in this way, we 
+# when its received, still solving our original system clock problem, but in this way, we
 # only give data when we have it -- instead of asking for data when we don't. Interesting stuff :)
-
 
 
 # imports
